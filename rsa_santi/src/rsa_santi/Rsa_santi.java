bbf3c74f728fca6835c4844b2d9fa52553b5a25c
santmartines <129224648+santmartines@users.noreply.github.com>
2023-11-22 00:22:24 -0600

    first commit

---
+++ rsa_santi/src/rsa_santi/Rsa_santi.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package rsa_santi;
+
+import GUI.CIFRAR;
+
+/**
+ *
+ * @author vic
+ */
+public class Rsa_santi {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        CIFRAR main = new CIFRAR();
+        main.setVisible(true);
+        main.setLocationRelativeTo(null);
+    }
+    
+}
